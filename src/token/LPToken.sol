7d0e302cafecbf4de2403d86e186cfe96950f56a
aizeka18 <a1izeka@gmail.com>
2026-05-17 15:05:11 +0500

    feat(token): add LP token for AMM liquidity shares

---
+++ src/token/LPToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LPToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("Astra LP Token", "ALP") Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
